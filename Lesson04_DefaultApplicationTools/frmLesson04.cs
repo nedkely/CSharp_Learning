0ac69733db7fd1f6c11b0ffadaff5b0a02c56678
nedkely <nedkely@gmail.com>
2024-04-25 06:13:58 +0430

    Lesson04

---
+++ Lesson04_DefaultApplicationTools/frmLesson04.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson04_DefaultApplicationTools
+{
+    public partial class frmLesson04 : Form
+    {
+        public frmLesson04()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
