4b235c3a308019ecbebb8af2fbd9f8e910dc767f
nedkely <nedkely@gmail.com>
2024-04-25 07:30:40 +0430

    Lesson 05 - the end

--- Lesson05_IfStatmentForStructures/frmMain.Designer.cs
+++ Lesson05_IfStatmentForStructures/frmMain.Designer.cs
@@ -29,21 +29,96 @@ namespace Lesson05_IfStatmentForStructures
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAct = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.lslValue = new System.Windows.Forms.ListBox();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.lstWhile = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnAct
+            // 
+            this.btnAct.Location = new System.Drawing.Point(45, 273);
+            this.btnAct.Name = "btnAct";
+            this.btnAct.Size = new System.Drawing.Size(265, 29);
+            this.btnAct.TabIndex = 0;
+            this.btnAct.Text = "IF Statment";
+            this.btnAct.UseVisualStyleBackColor = true;
+            this.btnAct.Click += new System.EventHandler(this.btnAct_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(45, 141);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(265, 27);
+            this.txtValue.TabIndex = 1;
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(385, 273);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(300, 29);
+            this.btnFor.TabIndex = 2;
+            this.btnFor.Text = "For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // lslValue
+            // 
+            this.lslValue.FormattingEnabled = true;
+            this.lslValue.ItemHeight = 20;
+            this.lslValue.Location = new System.Drawing.Point(385, 24);
+            this.lslValue.Name = "lslValue";
+            this.lslValue.Size = new System.Drawing.Size(300, 144);
+            this.lslValue.TabIndex = 3;
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(691, 273);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(300, 29);
+            this.btnWhile.TabIndex = 4;
+            this.btnWhile.Text = "While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // lstWhile
+            // 
+            this.lstWhile.FormattingEnabled = true;
+            this.lstWhile.ItemHeight = 20;
+            this.lstWhile.Location = new System.Drawing.Point(691, 24);
+            this.lstWhile.Name = "lstWhile";
+            this.lstWhile.Size = new System.Drawing.Size(300, 144);
+            this.lstWhile.TabIndex = 3;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1100, 450);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.lstWhile);
+            this.Controls.Add(this.lslValue);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnAct);
+            this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAct;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.ListBox lslValue;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.ListBox lstWhile;
     }
 }
 
--- Lesson05_IfStatmentForStructures/frmMain.cs
+++ Lesson05_IfStatmentForStructures/frmMain.cs
@@ -21,5 +21,77 @@ namespace Lesson05_IfStatmentForStructures
         {
 
         }
+
+        private void btnAct_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtValue.Text))
+            {
+                MessageBox.Show("textbox is null or empty");
+            }
+            else
+            {
+
+
+                var txt = this.txtValue.Text;
+                if (txt == "1")
+                {
+                    MessageBox.Show("number is 1");
+                }
+                else if (txt == "2")
+                {
+                    MessageBox.Show("number is 2");
+                }
+                else if (txt == "3")
+                {
+                    MessageBox.Show("number is 3");
+                }
+                else
+                {
+                    MessageBox.Show("number is something else");
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+            switch (this.txtValue.Text)
+            {
+                case "hamed":
+                    MessageBox.Show("value is hamed");
+                    break;
+                case "hamid":
+                    MessageBox.Show("value is hamid");
+                    break;
+                case "farhad":
+                    MessageBox.Show("value is farhad");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                this.lslValue.Items.Add("item number = " + i.ToString());
+            }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            while (this.lslValue.Items.Count > 0)
+            {
+                this.lstWhile.Items.Add(this.lslValue.Items[0].ToString());
+                this.lslValue.Items.RemoveAt(0);
+            }
+        }
     }
 }
