f7e0f8e12aaf68053850599d071f1f2f28961863
nedkely <nedkely@gmail.com>
2024-04-25 07:10:10 +0430

    lesson 05 - start

---
+++ Lesson05_IfStatmentForStructures/frmMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson05_IfStatmentForStructures
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
